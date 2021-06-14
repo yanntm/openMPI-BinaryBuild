1e0db9426488da24fcbd428a508071cfeb319293
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-06-14 17:39:52 +0200

    folder

--- build_openmpi.sh
+++ build_openmpi.sh
@@ -8,7 +8,7 @@ wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.1.tar.gz
 
 tar zxf openmpi-4.1.1.tar.gz
 
-cd openmpi*
+cd openmpi-4.1.1
 
 ./configure --enable-mpi-java --prefix=$PWD/usr/local/
 
